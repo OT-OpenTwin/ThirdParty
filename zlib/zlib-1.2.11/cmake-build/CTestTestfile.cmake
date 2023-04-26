d1164e09b125e0e2daa25d7ff4528e9723d45ecf
Peter Thoma <peter_thoma@outlook.com>
2023-04-26 10:38:08 +0200

    added zlib 3rd party library

---
+++ zlib/zlib-1.2.11/cmake-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Daten/Projekte/SimulationPlatform/Third_Party_Libraries/zlib/zlib-1.2.11
+# Build directory: C:/Daten/Projekte/SimulationPlatform/Third_Party_Libraries/zlib/zlib-1.2.11/cmake-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(example "example")
