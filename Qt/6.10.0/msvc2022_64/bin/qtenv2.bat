4faf625674fec18d3e807a565f96e8c4664bde54
Alexander Küster <alexk95@users.noreply.github.com>
2025-10-11 22:13:40 +0200

    Added Qt 6.10.0 and rebuild qt based projects (Part 9)

---
+++ Qt/6.10.0/msvc2022_64/bin/qtenv2.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Setting up environment for Qt usage...
+set PATH=C:\OpenTwin\ThirdParty\Qt\6.10.0\msvc2022_64\bin;%PATH%
+cd /D C:\OpenTwin\ThirdParty\Qt\6.10.0\msvc2022_64
+echo Remember to call vcvarsall.bat to complete environment setup!
